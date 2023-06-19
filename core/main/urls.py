a25799a1d4bf9e065572877a691f59c03b3cfc46
TuralHasanov11 <hasanovtural11@gmail.com>
2023-06-19 21:25:55 +0200

    rm reverse

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.utils.translation import gettext_lazy as _
 from main import views
 
 app_name = "main"
@@ -9,7 +8,8 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
     path('career', views.CareerListView.as_view(), name='career'),
-    path('terms-and-conditions', views.termsAndConditions, name='terms-and-conditions'),
+    path('terms-and-conditions', views.termsAndConditions,
+         name='terms-and-conditions'),
     path('privacy-policy', views.privacyPolicy, name='privacy-policy'),
     path('return-policy', views.returnPolicy, name='return-policy'),
     path('faq', views.FAQListView.as_view(), name='faq'),
